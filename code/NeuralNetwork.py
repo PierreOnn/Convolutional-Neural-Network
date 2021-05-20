f10b193d2127a133edce1e2741383cca79583584
PrinceWallyy <63965775+PrinceWallyy@users.noreply.github.com>
2021-05-20 16:29:48 +0200

    cnn adapted as proposed in slides

--- code/NeuralNetwork.py
+++ code/NeuralNetwork.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import numpy as np
 import math
 
+
 # Sigmoid activation function
 def sigmoid(input):
     new_input = np.zeros(input.shape)
@@ -17,13 +18,26 @@ def tanh(input):
 
 
 class CNN():
-    def __init__(self, Wxh, Why, Whh):
-        self.Wxh = Wxh
-        self.Why = Why
-        self.Whh = Whh
+    def __init__(self, D_in, H, D_out, N):
+        self.D_in = D_in
+        self.H = H
+        self.D_out = D_out
+        self.N = N
 
-    def feed_forward(self, x):
-        h_x = np.dot(x, self.Wxh)
-        h = tanh(h_x)
-        y = np.dot(h, self.Why)
-        return y
+    def feed_forward(self, x, y):
+        model = nn.Sequential(
+            nn.Linear(self.D_in, self.H),
+            nn.ReLU(),
+            nn.Linear(self.H, self.D_out),
+            nn.Sigmoid())
+        loss_fn = nn.MSELoss(reduction='sum')
+        learning_rate = 1e-4
+        optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+        for epoch in range(50):
+            y_pred = model(x)
+            loss = loss_fn(y_pred, y)
+            print('epoch: ', epoch, ' loss: ', loss.item())
+            optimizer.zero_grad
+            loss.backward()
+            optimizer.step()
+        return
--- code/main.py
+++ code/main.py
@@ -1,6 +1,12 @@
-def main():
-    print('Hello')
+from NeuralNetwork import *
 
+
+def main():
+    D_in, H, D_out, N = 10, 5, 1, 10
+    cnn = CNN(D_in, H, D_out, N)
+    x = torch.randn(N, D_in)
+    y = torch.randn(N, D_out)
+    print(cnn.feed_forward(x, y))
 
 if __name__ == '__main__':
     main()
